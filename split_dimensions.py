e7dae2e3d310c2ca2b04087852129996ff213d89
jose-marquez89 <jlmarquez89@gmail.com>
2021-02-02 10:56:28 -0600

    completes the split_fact function

--- split_dimensions.py
+++ split_dimensions.py
@@ -16,10 +16,30 @@ def split_dimension(fact: pd.DataFrame, id_name: str,
 def split_fact(fact_table: pd.DataFrame,
                dimensions: dict,
                reduce_size: bool = True,
-               sample_size: int = 1000) -> list:
-    """Splits musco fact table into a proper star schema"""
+               sample_size: int = 1000,
+               reduced_cols: list = None) -> list:
+    """
+    Splits fact table into a proper star schema.
+    Returns a fact table with 1000 samples by default.
+    """
 
     if reduce_size:
         fact_table = fact_table.sample(sample_size,
                                        replace=False,
                                        random_state=42)
+    
+    dataframes = []
+
+    for id, cols in dimensions.items():
+        dimension = split_dimension(fact_table, id, cols)
+        dataframes.append(dimension)
+
+        # merge id into dimension
+        fact_table = fact_table.merge(dimension, on=cols, how="inner")
+    
+    if reduced_cols is not None:
+        fact_table = fact_table[reduced_cols]
+
+    dataframes.append(fact_table)
+
+    return dataframes
